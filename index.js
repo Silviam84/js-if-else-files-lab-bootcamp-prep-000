76c7b2959793a0f466d1047b0bc14cfac869f035
classy-waterfall-6294 <silviamendoza223@yahoo.com>
2019-04-27 00:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 lesl} else {
   console.log("Expect the name to be Susan") {
-    
-    
-  
-}
-s
+  }
+
 
 
 // Don't worry about this
